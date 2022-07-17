ae08ffd1cc72aed06114af6647f1f933105a813e
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2022-07-17 20:18:57 +0300

    add hooks to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,99 +1,78 @@
-import { Component} from "react";
-// import { useState, useEffect } from "react";
+// import { Component} from "react";
+import { useState, useEffect, useRef } from "react";
 import ContactForm from "./ContactForm";
 import ContactList from "./ContactList";
 import Filter from "./Filter";
 import s from './App.module.css'
 
-// const exmConatcts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
+const exmConatcts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-//export function App() {
-//   const [contacts, setContacts] = useState(exmConatcts);
-//   const [filter, setFilter] = useState('');
-// }
+export function App() {
+  const [contacts, setContacts] = useState(exmConatcts);
+  const [filter, setFilter] = useState('');
+  const noSetLocalStorage = useRef(true);
 
-
-export class App extends Component {
+  useEffect(() => {
+    const contactsStor = localStorage.getItem('contacts');
+    const parsetContacts = JSON.parse(contactsStor);
+    if (parsetContacts !== null) {
+      setContacts(parsetContacts)
+    }
+  }, []);
   
-  state = {
-    contacts: [
-          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-  }
-
-   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsetContacts = JSON.parse(contacts);
-    if (parsetContacts) {
-      this.setState({ contacts: parsetContacts})
+  useEffect(() => {
+     if (noSetLocalStorage.current) {
+      noSetLocalStorage.current = false;
+      return;
     }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = (newContacts) => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  
+  const addContact = (newContacts) => {
     if (
-      this.state.contacts.find(contact =>{
+      contacts.find(contact => {
         return contact.name.toLowerCase().trim() === newContacts.name.toLowerCase().trim();
       })
-      ) {
-        alert(`${newContacts.name} is already in contacts`);
-        return;
-      }
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContacts],
-      }));
+    ) {
+      alert(`${newContacts.name} is already in contacts`);
+      return;
     }
+    setContacts(prevContacts => [...prevContacts, newContacts]);    
+  };
     
-  onChangeFilter = evt => {
-    this.setState({
-      filter: evt.currentTarget.value,
-    });
+  const onChangeFilter = evt => {
+      setFilter(evt.currentTarget.value);    
   };
 
-  findContact = () => {
-    const { contacts, filter } = this.state;
+  const findContact = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().trim().includes(filter.toLowerCase().trim())
     );
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-    // console.log(id);
+  const handleDeleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { filter } = this.state;
-    return (
+  return (
       <div className={s.container}>
         <h2>Phonebook</h2>
-        <ContactForm  addContact={this.addContact} />
+        <ContactForm  addContact={addContact} />
         <h2>Contacts</h2>
-        <Filter onChangeFilter={ this.onChangeFilter} filter={filter} />
+        <Filter onChangeFilter={onChangeFilter} filter={filter} />
         <ContactList
-          contacts={this.findContact()}
-          deleteContact={this.handleDeleteContact}
+          contacts={findContact()}
+          deleteContact={handleDeleteContact}
            /> 
       </div>
     );
-  }
 };
+
 
 
 
